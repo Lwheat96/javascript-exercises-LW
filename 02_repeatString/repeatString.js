e903bdbfa3ce8a7154b919a450a8f9a916a89b31
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-10 20:44:33 -0400

    Struggled with this one and has to look up the repeat() function.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    return str.repeat(num);
+    
 };
+repeatString('hey', 3);
+
+/*for (str = 0; str < num; str++){
+     if (str < 4)
+        str++;
+        return str;
+    };*/
 
 // Do not edit below this line
+
 module.exports = repeatString;
