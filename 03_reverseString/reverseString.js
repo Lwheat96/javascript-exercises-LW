8521dcd6e60319e7eeff221d4a95528fcbe84a18
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-11 20:38:56 -0400

    These exercises are really getting me.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function() {
+  let str = 'hello';
+  Array.from(str);
 
+  return str.type;
 };
+// I am really struggling with these exercises.
+/*Array.splice(str);
+Array.reverse(str);*/
 
+reverseString()
 // Do not edit below this line
 module.exports = reverseString;
