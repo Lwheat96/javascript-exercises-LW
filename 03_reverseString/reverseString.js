ad160ca999e88ec687c35f66381df2c9c303e38b
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-13 08:06:42 -0400

    Very close here but cant get rid of the commas in the reverseStr.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,13 +1,20 @@
+let str = 'hello'
 const reverseString = function() {
-  let str = 'hello';
-  Array.from(str);
+  for (let i = 0; i < str.length; i++){
+    let strReverse = str.split("");
+    strReverse.reverse();
+    let strR = strReverse.join();
+    return strR.toString(" ");
+}};
 
-  return str.type;
-};
+
+
 // I am really struggling with these exercises.
 /*Array.splice(str);
 Array.reverse(str);*/
 
 reverseString()
+
+console.log(str.type);
 // Do not edit below this line
 module.exports = reverseString;
