6f0efe09e9fb028fbfd12fed652ed30c13e9f4cb
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-13 09:34:52 -0400

    Man I can not get the commas out

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,15 +1,42 @@
-let str = 'hello'
 const reverseString = function() {
+  let str = 'hello';
     for (let i = 0; i < str.length; i++) {
       let strReverse = str.split("");
-          strReverse.reverse();
-      let strR = strReverse.toString();
-          strR.concat();
-          str.replaceAll(",", "");
+        strReverse.reverse();
+      let strR = strReverse.toString() 
+        strR.concat();
+        str.replaceAll(",", "");
       return strR;
     }
   
-}
+};
+
+reverseString();
+/* Psuedo
+1) I want to creat a function that 
+  a) takes a string;
+  b) turns string into an array
+  c) reverses that array
+  d) turns that reversed array back to a string
+
+
+I am having issues when turning the array back to a string
+*/
+
+/*const reverseString = function() {
+  let str = 'hello';
+    for (let i = 0; i < str.length; i++) {
+      let strReverse = str.split("");
+          strReverse.reverse();
+      let strR = strReverse.toString() 
+        strR.concat();
+        str.replaceAll(",", "");
+      return strR;
+    }
+  
+};
+*/
+
 /* for (let i = 0; i < str.length; i++) {
       let strReverse = str.split("");
           strReverse.reverse();
@@ -27,6 +54,6 @@ const reverseString = function() {
 /*Array.splice(str);
 Array.reverse(str);*/
 
-reverseString()
+
 // Do not edit below this line
 module.exports = reverseString;
