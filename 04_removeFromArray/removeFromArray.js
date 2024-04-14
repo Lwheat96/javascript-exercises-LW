14c6f5c11bdb5cce2149a61f6ed6bc56c59e5ef0
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-14 15:41:03 -0400

    Working on removeFromArray - Getting close I think.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+ const newArray = array.pop([]);
+ return array;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
