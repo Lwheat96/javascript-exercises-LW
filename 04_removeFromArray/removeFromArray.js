892a5472a8ac033cc2216f2fccfe0c6684bf6498
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-16 20:23:26 -0400

    I gave up and looked at the solution and still didnt exactly get it. Looks like I need to go back and study some more. This is going slow. But it is okay cause I am learning.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
 const removeFromArray = function(array) {
-    return array;
+    // need a new variable called newArray. Need an if statement.
 };
 
-removeFromArray([1, 2, 3, 4]);
+removeFromArray([1, 2, 3, 4], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
