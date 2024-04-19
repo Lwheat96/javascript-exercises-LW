63045a933b7456bc9b741d512d5b3873cf0915cb
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-19 12:31:54 -0400

    Accidental commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,10 @@
-const removeFromArray = function(object, ...array) {
+const removeFromArray = function(array, object) {
     // need a new variable called newArray. Need an if statement.
     const newArray = array.toSpliced(object);
     return newArray;
   };
   
- removeFromArray([1, 2, 3, 4], 3);
+ removeFromArray([1, 2, 3, 4]);
 //How can I use a loop to figure this problem out..
 
 // Do not edit below this line
