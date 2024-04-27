a23223138bcc1f09a03e275978c0c14e56b01fcf
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-27 18:31:43 -0400

    Did this on a replit... Not working though. Just updating this commit.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+let i;
+let number;
 
-};
+const sumAll = function(i, number) {
+    let total = i + number;
+    for (; i < number; i++) {
+      if (i < number) {
+        i + number;
+        i++;
+        console.log(i)
+      } else if (i = number) {
+  
+        return total;
+    };
+      return total
+    }
+  }
+  console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
